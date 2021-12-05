2095169f7b0496df7dbebc25646605592e90e0ac
StanSm1th <raman.mantas@gmail.com>
2021-12-05 11:44:59 +0200

    Added Logger and some methods

--- src/main/java/testingRecap/Calculator.java
+++ src/main/java/testingRecap/Calculator.java
@@ -1,4 +1,22 @@
 package testingRecap;
 
+import org.apache.log4j.Logger;
+
 public class Calculator {
+
+    private static final Logger LOGGER = Logger.getLogger(Calculator.class);
+
+    public int add (int firstNumber, int secondNumber){
+        LOGGER.info("Adding two numbers " + firstNumber + " and " + secondNumber);
+        return firstNumber + secondNumber;
+    }
+
+    public int addMultipleNumbers (int[] numbers) {
+        int totalSum = 0;
+        for (int number : numbers) {
+            totalSum += number;
+        }
+
+        return totalSum;
+    }
 }
---
+++ src/test/java/testingRecap/CalculatorTest.java
@@ -0,0 +1,15 @@
+package testingRecap;
+
+import org.junit.jupiter.api.Test;
+
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addMultipleNumbers() {
+    }
+}
